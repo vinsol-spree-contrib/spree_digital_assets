9f4eb0c04bd81458dd82c97281fd5a98ebef6619
Siddharth Sharma <siddharth10104737@gmail.com>
2016-03-02 11:31:31 +0530

    MAke build from digital_asset private

--- app/models/spree/asset_decorator.rb
+++ app/models/spree/asset_decorator.rb
@@ -3,13 +3,14 @@ Spree::Asset.class_eval do
 
   before_validation :build_from_digital_asset, if: 'digital_asset_id.present?'
 
-  def build_from_digital_asset
-    digital_asset = Spree::DigitalAsset.find_by(id: digital_asset_id)
-    if digital_asset.present?
-      self.attachment = digital_asset.attachment
-    else
-      errors.add(:base, 'invalid digital_asset_id passed')
+  private
+    def build_from_digital_asset
+      digital_asset = Spree::DigitalAsset.find_by(id: digital_asset_id)
+      if digital_asset.present?
+        self.attachment = digital_asset.attachment
+      else
+        errors.add(:base, 'invalid digital_asset_id passed')
+      end
     end
-  end
 
 end
