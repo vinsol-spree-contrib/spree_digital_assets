4211ee383ef43d91d271306a76d01f8ebe987643
Siddharth Sharma <siddharth10104737@gmail.com>
2016-03-02 11:11:35 +0530

    Add reset new digital form js and change shown event to show

--- app/assets/javascripts/spree/backend/spree_digital_assets.js
+++ app/assets/javascripts/spree/backend/spree_digital_assets.js
@@ -70,10 +70,9 @@ $( document ).ready(function() {
     hide_loader();
   });
 
-  $("#upload_asset_modal").on("shown.bs.modal", function(){
-    $(this).find('#new_digital_asset_form')[0].reset();
-    $(this).find('.formError').remove();
-  })
+  $('body').on('show.bs.modal', '#upload_asset_modal', function(){
+    reset_new_digital_asset_form();
+  });
 
 });
 
@@ -84,4 +83,10 @@ function show_loader(selector){
 
 function hide_loader(){
   $('.loader').remove();
+}
+
+function reset_new_digital_asset_form(){
+  var $new_digital_asset_form = $('#new_digital_asset_form');
+  $new_digital_asset_form[0].reset();
+  $new_digital_asset_form.find('.formError').remove();
 }
