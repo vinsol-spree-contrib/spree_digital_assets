63760a67f3102d2344c32325dfe0756e4fcfc05d
Siddharth Sharma <siddharth10104737@gmail.com>
2016-03-02 23:55:07 +0530

    CR fixes build digital_asset in controller

--- app/controllers/spree/admin/digital_assets_controller.rb
+++ app/controllers/spree/admin/digital_assets_controller.rb
@@ -3,7 +3,7 @@ module Spree
 
     class DigitalAssetsController < ResourceController
 
-      before_action :filter_digital_assets_by_folder, only: :index
+      before_action :filter_digital_assets_by_folder, :build_digital_asset, only: :index
 
       def index
         @digital_assets = @digital_assets.page(params[:page])
@@ -15,6 +15,10 @@ module Spree
           if params[:folder_id].present? && (@current_folder = Spree::Folder.find_by(id: params[:folder_id]))
             @digital_assets = @digital_assets.where(folder: @current_folder.self_and_descendants)
           end
+        end
+
+        def build_digital_asset
+          @digital_asset = Spree::DigitalAsset.new(folder_id: @current_folder.try(:id))
         end
 
     end
