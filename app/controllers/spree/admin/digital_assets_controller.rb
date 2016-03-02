c5eaf4e9ea755b5f2c29583c6210d333552c05cb
Siddharth Sharma <siddharth10104737@gmail.com>
2016-03-02 11:20:25 +0530

    Minor fixes digital_assets controller

--- app/controllers/spree/admin/digital_assets_controller.rb
+++ app/controllers/spree/admin/digital_assets_controller.rb
@@ -3,17 +3,20 @@ module Spree
 
     class DigitalAssetsController < ResourceController
 
-      before_action :filter_digital_assets_by_folder, if: 'params[:folder_id].present?'
+      before_action :filter_digital_assets_by_folder, only: :index
 
       def index
         @digital_assets = @digital_assets.page(params[:page]).per(36)
         render 'view_more' if params[:page].to_i > 1
       end
 
-      def filter_digital_assets_by_folder
-        @current_folder = Spree::Folder.find_by(id: params[:folder_id])
-        @digital_assets = @digital_assets.where(folder: @current_folder.self_and_descendants) if @current_folder.present?
-      end
+      private
+        def filter_digital_assets_by_folder
+          if params[:folder_id].present?
+            @current_folder = Spree::Folder.find_by(id: params[:folder_id])
+            @digital_assets = @digital_assets.where(folder: @current_folder.self_and_descendants) if @current_folder.present?
+          end
+        end
 
     end
 
