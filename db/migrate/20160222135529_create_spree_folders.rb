5eca080cbe1a6abca9ffcbe9f3c5fd1f492fb699
Siddharth Sharma <siddharth10104737@gmail.com>
2016-03-03 00:57:36 +0530

    Update spree_folder migration

--- db/migrate/20160222135529_create_spree_folders.rb
+++ db/migrate/20160222135529_create_spree_folders.rb
@@ -1,10 +1,11 @@
 class CreateSpreeFolders < ActiveRecord::Migration
   def change
     create_table :spree_folders do |t|
-      t.references :parent
-      t.string     :name
-      t.integer    :lft
-      t.integer    :rgt
+      t.string :name
+      t.integer :parent_id, null: true, index: true
+      t.integer :lft, null: false, index: true
+      t.integer :rgt, null: false, index: true
+
       t.timestamps null: false
     end
   end
