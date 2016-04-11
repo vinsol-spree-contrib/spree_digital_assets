c3275c87cbd0ddf117fb7ae60a80b76cc073ae00
Sachin <sachin.mittal@vinsol.com>
2016-04-12 01:44:30 +0530

    Fix Cr changes

--- app/assets/javascripts/spree/backend/spree_digital_assets/asset_details.js
+++ app/assets/javascripts/spree/backend/spree_digital_assets/asset_details.js
@@ -5,26 +5,30 @@ function AssetDetails(selectors) {
 AssetDetails.prototype.init = function() {
   var _this = this;
   this.wrapper.on('click', 'div.asset-area', function(){
-  	var $img = $(this).find('img');
-    _this.wrapper.find('#asset-name').html($img.data('name'));
-    _this.wrapper.find('#file-size').html($img.data('size'));
-    _this.wrapper.find('#created-date').html($img.data('created-on'));
-    _this.wrapper.find('#modified-date').html($img.data('modified-on'));
-    _this.wrapper.find('#related-products').html(_this.setRelatedProducts($img.data('related-products')));
+    _this.setAttributes($(this).find('img'));
     _this.wrapper.find('.asset-details').removeClass('hide');
   });
-  _this.wrapper.find('.asset-details').addClass('hide');
+};
+
+AssetDetails.prototype.setAttributes = function($img) {
+  this.wrapper.find('#asset-name').html($img.data('name'));
+  this.wrapper.find('#file-size').html($img.data('size'));
+  this.wrapper.find('#created-date').html($img.data('created-on'));
+  this.wrapper.find('#modified-date').html($img.data('modified-on'));
+  this.wrapper.find('#related-products').html(this.setRelatedProducts($img.data('related-products')));
 };
 
 AssetDetails.prototype.setRelatedProducts = function(data) {
   var $relatedProducts = this.wrapper.find('#related-products').html('');
-  if(!Object.keys(data).length) {
+  if(!data.length) {
     $relatedProducts.html('None');
   }	
-  $.each(data, function(key, value) {
-    $relatedProducts.append($('<a>').attr('href', '/admin/products/' + key + '/edit').text(value).css('display', 'block'));
+  $.each(data, function(index, product) {
+    $relatedProducts.append($('<a>').attr('href', '/admin/products/' + product[0] + '/edit')
+      .text(product[1]).css('display', 'block'));
   });
 };
+
 $(function () {
   var selectors = {
     wrapper: $('#wrapper')
--- app/helpers/spree/admin/digital_assets_helper.rb
+++ app/helpers/spree/admin/digital_assets_helper.rb
@@ -4,7 +4,7 @@ module Spree
       def related_products(digital_asset)
         digital_asset.assets.map do |asset|
           [asset.viewable.product.name.parameterize, asset.viewable.product.name]
-        end.to_h
+        end
       end
     end
   end
