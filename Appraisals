f47af766323b0ffeb7af2e48b2d980e4725d4bfe
Naresh Sharma <naresh@vinsol.com>
2020-12-22 15:31:05 +0530

    upgrade to spree 4-1

--- Appraisals
+++ Appraisals
@@ -13,6 +13,11 @@ appraise 'spree-3-4' do
   gem 'rails-controller-testing', '~> 1.0.1'
 end
 
+appraise 'spree-4-1' do
+  gem 'spree', '~> 4.1'
+  gem 'rails-controller-testing', '~> 1.0.1'
+end
+
 appraise 'spree-master' do
   gem 'spree', github: 'spree/spree', branch: 'master'
   gem 'rails-controller-testing', '~> 1.0.1'
--- app/helpers/spree/admin/base_helper_decorator.rb
+++ app/helpers/spree/admin/base_helper_decorator.rb
@@ -51,12 +51,12 @@ Spree::Admin::BaseHelper.class_eval do
   end
 
   def asset_details(digital_asset)
-    { 
+    {
       id: digital_asset.id,
       name: digital_asset.name,
-      size: number_to_human_size(digital_asset.attachment_file_size), 
-      created_on: digital_asset.created_at.to_date.to_formatted_s(:long), 
-      modified_on: digital_asset.updated_at.to_date.to_formatted_s(:long), 
+      size: number_to_human_size(digital_asset.attachment.byte_size),
+      created_on: digital_asset.created_at.to_date.to_formatted_s(:long),
+      modified_on: digital_asset.updated_at.to_date.to_formatted_s(:long),
       related_products: related_products(digital_asset)
     }
   end
